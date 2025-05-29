8b6067c8597e500070d585adbcb177066ad818f0
RainerHeintzmann <heintzmann@gmail.com>
2025-05-29 16:56:39 +0200

    added hyperbolic modifyers

--- src/modifyers.jl
+++ src/modifyers.jl
@@ -1,17 +1,34 @@
-export Fixed, Positive, Normalize, ClampSum, BoundedSoftMax
+export Fixed, Normalize, ClampSum, BoundedSoftMax, Positive, PositiveH, BoundedSoftMaxH
 
 abstract type Modificator end
+
+function Base.size(d::Modificator, varargs...)
+    return size(d.data, varargs...)
+end
+
+# Functions which all Modificators should implement:
+# These are the bare fallback versions:
+# by default all Modificators are not fixed, i.e. they are part of the fit:
+is_fixed(val) = false
+# this returns just the bare value
+get_val(val) = val
+get_fwd_val(val) = val
+get_fwd_val(val, id, fit_params, non_fit) = get_fwd_val(fit_params[id]) # @view fit_params[id]
+# inverse opterations for the pre-forward model parts
+get_inv_val(val) = val
+get_inv_val(val, fct) = fct(val)
 
 # this datatyp specifies that this parameter is not part of the fit
 struct Fixed{T} <: Modificator
     data::T
 end
+# if Fixed appears anywhere in a chain of modifyers, all of them are ignored
+is_fixed(val::Fixed) = true
+get_val(val::Fixed) = get_val(val.data) # to strip off the properties
+get_fwd_val(val::Fixed) = get_fwd_val(val.data) # to strip off the properties
+get_fwd_val(val::Fixed, id, fit, non_fit) = getindex(non_fit, id) # do not apply any modifiers here.
+get_inv_val(val::Fixed, fct=identity) = get_inv_val(val.data, fct)
 
-# this datatyp specifies that this parameter is kept positive during the fit
-# this is achieved by introducing an auxiliary function whos abs2.() yields the parameter
-struct Positive{T} <: Modificator
-    data::T
-end
 
 """  Normalize{T}
     this datatyp specifies that this parameter is normalized before the fit 
@@ -24,8 +41,16 @@ struct Normalize{T,F} <: Modificator
     factor::F
 end
 
-function Base.size(d::Modificator, varargs...)
-    return size(d.data, varargs...)
+is_fixed(val::Normalize) = is_fixed(val.data)
+get_val(val::Normalize) = get_val(val.data) # to strip off the properties
+get_fwd_val(val::Normalize) = get_fwd_val(val.data) .* val.factor
+get_fwd_val(val::Normalize, id, fit, non_fit) = get_fwd_val(val.data, id, fit, non_fit) .* val.factor
+get_inv_val(val::Normalize, fct=identity) = get_inv_val(val.data, (x)->fct.(x)./ val.factor) 
+
+## helper function for ClampSum
+function clamp_sum(val)
+    myval = get_inv_val(val)
+    return myval[1:end-1] # optimize the 1D- view with the last value missing
 end
 
 """ ClampSum{T}
@@ -37,6 +62,7 @@ struct ClampSum{T, S, N}
     mysum::S
     mysize::NTuple{N, Int}
 end
+
 """ ClampSum(dat::T, dims=ntuple((n)->n, ndims(dat))) where {T}
 
 convenience constructor. 
@@ -50,26 +76,10 @@ function ClampSum(dat::T) where {T}
     ClampSum{T,typeof(mysum),length(size(val))}(dat, mysum, size(val))
 end
 
-# if Fixed appears anywhere in a chain of modifyers, all of them are ignored
-is_fixed(val) = false
-is_fixed(val::Fixed) = true
-is_fixed(val::Positive) = is_fixed(val.data)
-is_fixed(val::Normalize) = is_fixed(val.data)
 is_fixed(val::ClampSum) = is_fixed(val.data)
-
-# access a NamedTuple with a symbol (eg  :a)
-# this returns just the bare value
-get_val(val) = val
-get_val(val::Fixed) = get_val(val.data) # to strip off the properties
-get_val(val::Positive) = get_val(val.data) # to strip off the properties
-get_val(val::Normalize) = get_val(val.data) # to strip off the properties
 get_val(val::ClampSum) = get_val(val.data) # to strip off the properties
 
 # evaluated the pre-forward model modifyers
-get_fwd_val(val) = val
-get_fwd_val(val::Fixed) = get_fwd_val(val.data) # to strip off the properties
-get_fwd_val(val::Positive) = abs2.(get_fwd_val(val.data)) 
-get_fwd_val(val::Normalize) = get_fwd_val(val.data) .* val.factor
 function get_fwd_val(val::ClampSum)
     myvals = get_fwd_val(val.data)
     # @show val.mysum
@@ -77,32 +87,33 @@ function get_fwd_val(val::ClampSum)
     reshape(vcat(myvals, val.mysum - sum(myvals)), val.mysize)
 end
 
-get_fwd_val(val::Fixed, id, fit, non_fit) = getindex(non_fit, id) # do not apply any modifiers here.
-get_fwd_val(val::Positive, id, fit, non_fit) = abs2.(get_fwd_val(val.data, id, fit, non_fit))
-get_fwd_val(val::Normalize, id, fit, non_fit) = get_fwd_val(val.data, id, fit, non_fit) .* val.factor
 function get_fwd_val(val::ClampSum, id, fit, non_fit)
     myvals = get_fwd_val(val.data, id, fit, non_fit)
-    # @show val.mysum
-    # @show sum(myvals)
     reshape(vcat(myvals, val.mysum - sum(myvals)), val.mysize)
 end
-get_fwd_val(val, id, fit_params, non_fit) = get_fwd_val(fit_params[id]) # @view fit_params[id]
 # begin 
 #     tmp = @view fit_params[id]#  getindex(fit, id)
 #     get_fwd_val(tmp)
 # end
-
-# inverse opterations for the pre-forward model parts
-get_inv_val(val) = val
-get_inv_val(val, fct) = fct(val)
-get_inv_val(val::Fixed, fct=identity) = get_inv_val(val.data, fct)
-get_inv_val(val::Positive, fct=identity) = get_inv_val(val.data, (x)->(sqrt.(fct.(max.(x, 0)))))
-get_inv_val(val::Normalize, fct=identity) = get_inv_val(val.data, (x)->fct.(x)./ val.factor) 
 get_inv_val(val::ClampSum, fct=identity) = get_inv_val(val.data, (x)->clamp_sum(fct.(x)))
-function clamp_sum(val)
-    myval = get_inv_val(val)
-    return myval[1:end-1] # optimize the 1D- view with the last value missing
+
+""" Positive{T}
+    this datatyp specifies that this parameter is positive during the fit
+    this is achieved by introducing an auxiliary function whos abs2.() yields the parameter
+    Note that the inverse operation will return the square root of the value, picking only the positive branch as the starting value.
+"""
+struct Positive{T} <: Modificator
+    data::T
 end
+is_fixed(val::Positive) = is_fixed(val.data)
+get_val(val::Positive) = get_val(val.data) # to strip off the properties
+get_fwd_val(val::Positive) = abs2.(get_fwd_val(val.data)) 
+get_fwd_val(val::Positive, id, fit, non_fit) = abs2.(get_fwd_val(val.data, id, fit, non_fit))
+get_inv_val(val::Positive, fct=identity) = get_inv_val(val.data, (x)->(sqrt.(fct.(max.(x, 0)))))
+
+# Helper functions for BoundedSoftMax:
+sigmoid(x) = 1 / (1 + exp(-x))
+logit(x) = log(x / (1 - x))
 
 """BoundedSoftMax{T}
     this datatyp specifies that parameter is bounded between `lower` and `upper` values. 
@@ -129,16 +140,64 @@ struct BoundedSoftMax{T} <: Modificator
 
 end
 
-
-# Helper functions:
-sigmoid(x) = 1 / (1 + exp(-x))
-logit(x) = log(x / (1 - x))
-
 is_fixed(val::BoundedSoftMax) = is_fixed(val.data)
-
 get_val(val::BoundedSoftMax) = get_val(val.data)
-
 get_fwd_val(val::BoundedSoftMax) = val.lower .+ (val.upper - val.lower) .* sigmoid.(get_fwd_val(val.data))
 get_fwd_val(val::BoundedSoftMax, id, fit, non_fit) = val.lower .+ (val.upper - val.lower) .* sigmoid.(get_fwd_val(val.data, id, fit, non_fit))
 
 get_inv_val(val::BoundedSoftMax, fct=identity) = get_inv_val(val.data, (x) -> logit.((fct.(x) .- val.lower) ./ (val.upper - val.lower)))
+
+
+#### Hyperbolic Versions of Positive and BoundedSoftMax:
+piecewise_hyperbolic(x) = ifelse(x < 0, 1/(1-x), 1+x)
+piecewise_hyperbolic_inv(x) = ifelse(x < 1, 1 - 1/x, x - 1)
+
+""" PositiveH{T}
+    this datatyp specifies that this parameter is positive during the fit
+    this is achieved by introducing an auxiliary function whos abs2.() yields the parameter
+    Note that the inverse operation will return the square root of the value, picking only the positive branch as the starting value.
+"""
+struct PositiveH{T} <: Modificator
+    data::T
+end
+is_fixed(val::PositiveH) = is_fixed(val.data)
+get_val(val::PositiveH) = get_val(val.data) # to strip off the properties
+get_fwd_val(val::PositiveH) = piecewise_hyperbolic.(get_fwd_val(val.data)) 
+get_fwd_val(val::PositiveH, id, fit, non_fit) = piecewise_hyperbolic.(get_fwd_val(val.data, id, fit, non_fit))
+get_inv_val(val::PositiveH, fct=identity) = get_inv_val(val.data, (x)->(piecewise_hyperbolic_inv.(fct.(max.(x, 0)))))
+
+# Helper functions for BoundedSoftMax:
+sigmoid_hyperbolic(x) = 1 / (1 + piecewise_hyperbolic(-x))
+sigmoid_hyperbolic_inv(x) = piecewise_hyperbolic_inv(x / (1 - x))
+
+"""BoundedSoftMaxH{T}
+    this datatyp specifies that parameter is bounded between `lower` and `upper` values. 
+    Achieved with sigmoid function to map the optimizer variable to the range [lower, upper].
+    The inverse mapping is done by using the logit function.
+"""
+struct BoundedSoftMaxH{T} <: Modificator
+    data::T
+    lower::eltype(T)
+    upper::eltype(T)
+
+    function BoundedSoftMaxH(dat::T, lower=zero(eltype(T)), upper=one(eltype(T))) where {T}
+        if lower >= upper
+            throw(ArgumentError("lower bound must be smaller than upper bound"))
+        end
+        if any(dat .< lower) 
+            throw(ArgumentError("all data must be greater than or equal to lower bound"))
+        end
+        if any(dat .> upper) 
+            throw(ArgumentError("all data must be less than or equal to upper bound"))
+        end
+        return new{T}(dat, lower, upper)
+    end
+
+end
+
+is_fixed(val::BoundedSoftMaxH) = is_fixed(val.data)
+get_val(val::BoundedSoftMaxH) = get_val(val.data)
+get_fwd_val(val::BoundedSoftMaxH) = val.lower .+ (val.upper - val.lower) .* sigmoid_hyperbolic.(get_fwd_val(val.data))
+get_fwd_val(val::BoundedSoftMaxH, id, fit, non_fit) = val.lower .+ (val.upper - val.lower) .* sigmoid_hyperbolic.(get_fwd_val(val.data, id, fit, non_fit))
+
+get_inv_val(val::BoundedSoftMaxH, fct=identity) = get_inv_val(val.data, (x) -> sigmoid_hyperbolic_inv.((fct.(x) .- val.lower) ./ (val.upper - val.lower)))
--- test/modifyers.jl
+++ test/modifyers.jl
@@ -16,6 +16,17 @@ end
     @test fixed_vals == InverseModeling.ComponentVector((tst2=15.0,))
 end
 
+@testset "Modifyer ClampSum" begin
+    val = rand(3,4)
+    tst_val = (tst1=ClampSum(val), )
+    tst_start_vals, fixed_vals, forward, backward, get_fit_results = create_forward((x)->x(:tst1), tst_val);
+
+    fwd = forward(tst_start_vals)
+    @test isapprox(fwd, val)
+    @test size(tst_start_vals.tst1) == (prod(size(val))-1,)
+    @test tst_start_vals.tst1[:] == val[1:end-1]
+end
+
 @testset "Modifyer Positive" begin
     val =15.0
     tst_val = (tst1=Positive(val), )
@@ -37,13 +48,32 @@ end
     @test tst_start_vals.tst1[1] ≈ 0
 end
 
-@testset "Modifyer ClampSum" begin
-    val = rand(3,4)
-    tst_val = (tst1=ClampSum(val), )
+@testset "Modifyer PositiveH" begin
+    val =15.0
+    tst_val = (tst1=PositiveH(val), )
     tst_start_vals, fixed_vals, forward, backward, get_fit_results = create_forward((x)->x(:tst1), tst_val);
 
     fwd = forward(tst_start_vals)
     @test isapprox(fwd, val)
-    @test size(tst_start_vals.tst1) == (prod(size(val))-1,)
-    @test tst_start_vals.tst1[:] == val[1:end-1]
+    @test tst_start_vals.tst1 > 0
+    @test tst_start_vals.tst1 ≈ 14
+
+    val = 0.001
+    tst_val = (tst1=PositiveH(val), )
+    tst_start_vals, fixed_vals, forward, backward, get_fit_results = create_forward((x)->x(:tst1), tst_val);
+    fwd = forward(tst_start_vals)
+    @test isapprox(fwd, val)
+    @test tst_start_vals.tst1 ≈ -999
 end
+
+@testset "Modifyer BoundedSoftMaxH" begin
+    val = [5f0, 0.2f0, 10f0]
+    tst_val = (tst1=BoundedSoftMaxH(val, 0, 20),)
+    tst_start_vals, fixed_vals, forward, backward, get_fit_results = create_forward((x)->x(:tst1), tst_val);
+    @test eltype(tst_start_vals.tst1) == Float32
+
+    fwd = forward(tst_start_vals)
+    @test isapprox(fwd, val)
+    @test tst_start_vals.tst1[3] ≈ 0
+end
+
